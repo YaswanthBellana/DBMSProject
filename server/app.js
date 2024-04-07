fa688a93f6b856321e1373b20f441120fec8d9b2
YaswanthBellana <yaswanth1847@gamail.com>
2024-04-07 20:49:10 +0000

    hi

--- server/app.js
+++ server/app.js
@@ -85,7 +85,7 @@ app.post("/:transport", async (request, response) => {
         return response.status(500).json({ error: "Internal Server Error" });
       }
       response.json(results);
-      console.log(results);    
+      console.log(results);
     });
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -203,30 +203,36 @@ app.post("/fg", async (request, response) => {
   });
 });
 
-app.post("/reviewPlace", async (request, response) => {
+app.get("/reviewPlace", async (request, response) => {
   try {
     const { selectedPlace, selectedSort } = request.body;
-    
-    // Modify your SQL query accordingly based on selectedPlace and selectedSort
-    const query = `SELECT r.review, r.rating, u.name, p.place_name
-    FROM reviews as r INNER JOIN users as u INNER JOIN places as p
-    WHERE (r.user_id = u.user_id) AND (p.place_id = r.place_id)
-    AND p.place_name = ? ORDER BY r.rating ${selectedSort};`;
 
-    db.all(query, [selectedPlace], (err, rows) => {
+    if (!selectedPlace || !selectedSort) {
+      return response
+        .status(400)
+        .json({ error: "Bad Request: Missing parameters" });
+    }
+
+    const query = `SELECT r.review, r.rating, u.name, p.place_name
+      FROM reviews as r
+      JOIN users as u ON r.user_id = u.user_id
+      JOIN places as p ON r.place_id = p.place_id
+      WHERE p.place_name = ? ORDER BY r.rating ${selectedSort};`;
+
+    db.get(query, [selectedPlace], (err, rows) => {
       if (err) {
         console.error("Error executing query:", err);
         return response.status(500).json({ error: "Internal Server Error" });
       }
       console.log("-------------");
+      console.log(rows);
       response.send({ reviews: rows });
     });
   } catch (error) {
     console.error("Error:", error);
-    response.status(500).json({ error });
+    response.status(500).json({ error: "Internal Server Error" });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
